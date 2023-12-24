6ec4c083d0bda84ccc7c1a440f3e47f1fd8b27ba
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2023-12-24 21:33:37 +0530

    9:33 24.12.23 create header
    created Header.js for styling

---
+++ react_test_site/src/Components/Header.js
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+function Header (){
+    return (
+        <>
+        <h1 className= {styles.blueBig} >Hello Styling</h1>
+        <p>Decorate ME</p>
+        </>
+        
+    )
+}
+
+export default Header;
