77c0fb35c4b507a0011478c61059b56f70843640
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2023-12-31 22:19:37 +0530

    update myform

--- react_test_site/src/Forms/MyForm.js
+++ react_test_site/src/Forms/MyForm.js
@@ -6,7 +6,7 @@ function MyForm (){
     // const [email,setEmail] = useState("")
     //console.log('Current State',name);
 
-    const [input, setInput] = useState({});
+    const [input, setInput] = useState({phone: "+94", about : "I am a"});
 
     function handleSubmit(e){
         e.preventDefault();
@@ -31,7 +31,18 @@ function MyForm (){
 
                 <label>Enter your name: <input type="text"  name="name" onChange={handleChange}/></label> <br/>
                 <label>Enter your age: <input type="text" name="age" onChange={handleChange}/></label> <br/>
-                <label>Enter your email: <input type="text" name="mail" onChange={handleChange}/></label> <br/>
+                <label>Enter your email: <input type="text" name="mail" value={input.email} onChange={handleChange}/></label> <br/>
+                <label>Enter your phone: <input type="text" name="phone" value={input.phone} onChange={handleChange}/></label> <br/>
+                <label>Select your country: 
+                    <select type="text" name="country" value={input.country} onChange={handleChange}>
+                        <option value={""}>...Select</option>
+                        <option value={"India"}>India</option>
+                        <option value={"USA"}>USA</option>
+                        <option value={"Sri Lanka"}>Sri Lanka</option>
+                        <option value={"Japan"}>Japan</option>
+                    </select>
+                    </label> <br/>
+                    <label>Tell about you:<br/> <textarea type="text" name="about" value={input.about} onChange={handleChange}/></label> <br/>
                 <label><input type="submit" value="Submit Form"/></label>
             </form>
         </>
