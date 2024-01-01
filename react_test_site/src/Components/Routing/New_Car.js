c205499f3cbf3a89dc356b6f7508319646e3989d
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-01-01 14:02:03 +0530

    nested routes

---
+++ react_test_site/src/Components/Routing/New_Car.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function New_car(){
+    return <h1>New Car</h1>
+}
---
+++ react_test_site/src/Components/Routing/Old_car.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Old_car(){
+    return <h1>Old Car</h1>
+}
